0f60193550671197e769bd7024bfc443aa47d489
unknown <30015@fiecalunos.lan>
2019-09-10 17:14:52 -0300

    Aula 10/09/2019

---
+++ dao/task-dao.js
@@ -0,0 +1,22 @@
+const db = require('./db');
+
+class TaskDAO {
+    constructor() {
+        this.db = db;
+    }
+    listar(userId) {
+        return new Promise((resolve, reject) => {
+            this.db.query("SELECT id,titulo,descricao,id_usuario, date_format(data, '%d/%m/%Y %H:%i') data_formatada FROM tarefas WHERE id_usuario = ? ORDER BY data,titulo",
+                [userId],
+                (erro, tarefas) => {
+                    if (erro) {
+                        return reject("Não foi possivel listar as tarefas. Erro: " + erro);
+                    }
+                    return resolve(tarefas);
+                }
+            )
+        });
+    }
+}
+
+module.exports = TaskDAO;
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+/*
+    Devemos importar a classe IndexController para que a rota index.js apenas "roteie" requisições. Quem deve se preocupar com o processamento das requisições é o controller
+*/
+
+const IndexController = require('../controller/index-controller');
+const indexController = new IndexController();
+
+var express = require('express');
+var router = express.Router();
+
+//chamados o método index() do controller index-controller.js
+router.get('/', indexController.index());
+
+router.get('/home', indexController.home());
+
+module.exports = router;
